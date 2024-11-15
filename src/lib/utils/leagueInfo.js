158c60a96e61e5bfd271fa7d1ba1e26aa4c11ff1
dkozi05 <kozidmitri@gmail.com>
2024-11-15 15:45:46 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -32,7 +32,7 @@ export const managers = [
       "location": null, // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/IMG_5821.jpeg", // square ratio recommended (no larger than 500x500)
-      "fantasyStart": 2020, // (optional) when did the manager start playing fantasy football
+      "fantasyStart": 2020, // (optional) when did the manager start playing fantas football
       "favoriteTeam": "kc", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
