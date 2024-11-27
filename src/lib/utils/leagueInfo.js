b474659e60369284d7c1d0e002925299ae058b3a
dkozi05 <kozidmitri@gmail.com>
2024-11-27 16:39:54 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -45,7 +45,7 @@ export const managers = [
       "philosophy": "",
       "tradingScale": 10, // 1 - 10
       "preferredContact": "Text", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
-    }
+    },
 
   {
       "managerID": "862895123120377856",  // ID of the roster that the manager manages (look at the order of the power rankings graph)
